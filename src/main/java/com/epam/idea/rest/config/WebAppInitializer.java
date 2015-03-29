715561b27a6143203bb043404f65045728674f45
Dzmitry Shylovich <Dzmitry_Shylovich@epam.com>
2015-03-29 09:57:41 +0000

    initialize spring bean profile through ContextLoaderListener in web.xml, delete unnesesary application.properties file

--- src/main/java/com/epam/idea/rest/config/WebAppInitializer.java
+++ src/main/java/com/epam/idea/rest/config/WebAppInitializer.java
@@ -1,14 +1,7 @@
 package com.epam.idea.rest.config;
 
-import java.io.IOException;
 import javax.servlet.Filter;
 
-import com.epam.idea.core.repository.config.support.DatabaseConfigProfile;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.io.support.ResourcePropertySource;
-import org.springframework.util.ObjectUtils;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -19,7 +12,7 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] {RootConfig.class};
+		return new Class[]{RootConfig.class};
 	}
 
 	@Override
@@ -38,27 +31,5 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 		encodingFilter.setEncoding("UTF-8");
 		encodingFilter.setForceEncoding(true);
 		return new Filter[]{encodingFilter};
-	}
-
-	@Override
-	protected WebApplicationContext createRootApplicationContext() {
-		Class<?>[] configClasses = getRootConfigClasses();
-		if (!ObjectUtils.isEmpty(configClasses)) {
-			AnnotationConfigWebApplicationContext rootAppContext = new AnnotationConfigWebApplicationContext();
-			rootAppContext.register(configClasses);
-			ConfigurableEnvironment environment = rootAppContext.getEnvironment();
-			try {
-				environment.getPropertySources().addFirst(new ResourcePropertySource("classpath:application.properties"));
-				environment.setActiveProfiles(environment.getRequiredProperty("spring.profiles.active"));
-				//todo add logs
-			} catch (IOException | IllegalStateException e) {
-				// it's ok if the file is not there or required property is absent. set default profile.
-				environment.setActiveProfiles(DatabaseConfigProfile.DEV);
-			}
-			return rootAppContext;
-		}
-		else {
-			return null;
-		}
 	}
 }
