351fb1f4ad14fe0f4e2be61d6a27c0fcd1b8dd65
Ihar_Niakhlebau <Ihar_Niakhlebau@epam.com>
2015-04-02 17:13:47 +0300

    Initialize tag colection in IdeaServiceImpl class, method findIdeasByTagId

--- src/main/java/com/epam/idea/core/service/impl/IdeaServiceImpl.java
+++ src/main/java/com/epam/idea/core/service/impl/IdeaServiceImpl.java
@@ -84,7 +84,7 @@ public class IdeaServiceImpl implements IdeaService {
 	public List<Idea> findIdeasByTagId(final long tagId) {
 		List<Idea> ideas = ideaRepository.findByTagId(tagId);
 		ideas.forEach(idea -> {
-			idea.getRelatedTags().size();
+			Hibernate.initialize(idea.getRelatedTags());
 		});
 		return ideas;
 	}
