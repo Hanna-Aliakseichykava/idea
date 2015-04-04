995fc1efaf4b9eac6af9e06c83601ec8b74d4cfe
Dzmitry Shylovich <Dzmitry_Shylovich@epam.com>
2015-04-05 01:50:47 +0300

    Create new WebAppUnitTest annotation
    We are repeating the same configuration across our JUnit-based test
    suite. WebAppUnitTest is a custom composed annotation that
    centralizes the common unit test configuration which is intended to
    reduce duplication.

---
+++ src/test/java/com/epam/idea/rest/annotation/WebAppUnitTest.java
@@ -0,0 +1,34 @@
+package com.epam.idea.rest.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.epam.idea.rest.config.TestRootConfig;
+import com.epam.idea.rest.config.WebAppConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * {@code @WebAppUnitTest} is a composed class-level annotation that centralizes
+ * the common unit test configuration for web layer.
+ *
+ * Note that {@code @WebAppUnitTest} must be used in conjunction with
+ * {@code @RunWith(SpringJUnit4ClassRunner.class)}, either within a single test class
+ * or within a test class hierarchy.
+ *
+ * @see org.springframework.web.context.WebApplicationContext
+ * @see org.springframework.test.context.ContextHierarchy
+ * @see org.springframework.test.context.ContextConfiguration
+ * @see org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ContextHierarchy({
+		@ContextConfiguration(classes = TestRootConfig.class),
+		@ContextConfiguration(classes = WebAppConfig.class)
+})
+@WebAppConfiguration
+public @interface WebAppUnitTest {}
